2a8bed5982f1a370f3c02aff24c020afd9b5aa7b
luxuze <luxuze@icloud.com>
2019-08-18 19:56:30 +0800

    2019-08-18 19:56:30.001205 +0800 CST m=+1.768339823

--- utils/cronTask.go
+++ utils/cronTask.go
@@ -9,22 +9,25 @@ import (
 	"github.com/robfig/cron"
 )
 
-func task() {
-	fmt.Println("cron ...")
-	now := time.Now()
-	cmd := fmt.Sprintf(`
-	    git add -A &&
+const (
+	shellToUse = "bash"
+	quickPush  = `
+		git add -A &&
 		git commit -m "%v" &&
 		git push;
-	`, now)
-	fmt.Println(cmd)
+	`
+)
+
+func task() {
+	fmt.Println("golang cron task running ...")
+	cmd := fmt.Sprintf(quickPush, time.Now())
 	err, std, stderr := ShellCmd(cmd)
 	fmt.Println(err, std, stderr)
 }
 
-const shellToUse = "bash"
-
 func ShellCmd(command string) (error, string, string) {
+	fmt.Println(command)
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd := exec.Command(shellToUse, "-c", command)
