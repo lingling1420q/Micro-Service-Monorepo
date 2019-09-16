63d3d714a08d0e326965f3a7a4091cfb185a4224
Xuze Lu <luxuze@agora.io>
2019-09-16 15:41:32 +0800

    print parsed body

--- routes/print/print.go
+++ routes/print/print.go
@@ -4,6 +4,7 @@ import (
 	http "net/http"
 	logger "gin-demo/logger"
 	gin "github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
 func IndexGet(c *gin.Context) {
@@ -16,9 +17,11 @@ func IndexGet(c *gin.Context) {
 
 func IndexPost(c *gin.Context) {
 	query := c.Request.URL.Query()
-	form := c.PostFormMap("")
-	logger.Info(query)
-	logger.Info(form)
+	logger.Infof("Query: %v", query)
+	c.Request.ParseForm()
+	logger.Infof("JsonForm: %v", c.Request.PostForm )
+	data, _ := ioutil.ReadAll(c.Request.Body)
+	logger.Infof("BodyForm: %v", string(data))
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
